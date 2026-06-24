3971f64312f744a771990cbf720a17e6d4378bc8
imsamaritan <aicodemate.talk@gmail.com>
2026-06-24 19:51:14 +0200

    Create .env config and connecting to mysql db
    - Create .env at root/top level of the project dir
    - Added definions for Database
    - Use vlucas/phpdotenv to populate $_ENV with .env file config
    - Test connection using Home controller and Home/Index view

--- src/App/Controllers/Home.php
+++ src/App/Controllers/Home.php
@@ -7,16 +7,22 @@ namespace App\Controllers;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\PhpRenderer;
+use App\Database;
 
 class Home
 {
-    public function __construct(private PhpRenderer $renderer)
+    public function __construct(private PhpRenderer $renderer, private Database $database)
     {
     }
 
     public function __invoke(Request $request, Response $response): Response
     {
-        $this->renderer->render($response, "Home/Index.phtml", ["title" => "Home Page!"]);
+        $sql = "SELECT * FROM users";
+        $connection = $this->database->connect();
+        $statement = $connection->query($sql);
+        $statement->execute();
+        $count = $statement->rowCount();
+        $this->renderer->render($response, "Home/Index.phtml", ["title" => "Home Page!", "count" => $count]);
         return $response;
     }
 }
