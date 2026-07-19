a166cd1e9b1f57ce8012b91ea8dc7314d4fb29b8
imsamaritan <aicodemate.talk@gmail.com>
2026-07-20 00:20:30 +0200

    Implementing Repository pattern
    - Create interface for Use repository
    - Implemented creating user into mysql and returned a payload (id, role,
      account pending status)
    - Implemented getUser method into User Repository
    - Added a definition for UserRepositoryInterface which uses DI\get to
      load a new up User Repository class DI\get(User::class)
    - User Repository implemented the UserRepositoryInterface
    - Use UserRepositoryInterface in users controller as type reference for
      users constructor

--- config/definitions.php
+++ config/definitions.php
@@ -7,6 +7,8 @@ require_once dirname(__DIR__) . "/helper/constant-variables-helper.php";
 use Slim\Views\PhpRenderer;
 use Dotenv\Dotenv;
 use App\Database;
+use App\Interfaces\UserRepositoryInterface;
+use App\Repositories\Users\User;
 
 $dotenv = Dotenv::createImmutable(ROOT_PATH);
 $dotenv->safeLoad();
@@ -19,10 +21,11 @@ return [
     },
     Database::class => function () {
         return new Database(
-            host:       $_ENV["DB_HOST"],
-            user:       $_ENV["DB_USER"],
-            password:   $_ENV["DB_PASSWORD"],
-            database:   $_ENV["DB_NAME"],
+            host: $_ENV["DB_HOST"],
+            user: $_ENV["DB_USER"],
+            password: $_ENV["DB_PASSWORD"],
+            database: $_ENV["DB_NAME"],
         );
     },
+    UserRepositoryInterface::class => DI\get(User::class),
 ];
