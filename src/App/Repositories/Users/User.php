8a1bf054a0608f6dc40f6c6391cab45023067116
imsamaritan <aicodemate.talk@gmail.com>
2026-07-19 14:45:55 +0200

    Created a user to mysql.
    - Create Global or base Repository class for handling Repositories tasks
    - Create Users repository with create method for creating a user to
      mysql

--- src/App/Repositories/Users/User.php
+++ src/App/Repositories/Users/User.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace App\Repositories\Users;
 
 use App\Database;
+use App\Repositories\Repository;
 
-class User
+class User extends Repository
 {
     public function __construct(private Database $database)
     {
@@ -14,6 +15,27 @@ class User
 
     public function create(array $user): array
     {
-        return $user;
+        $table = $this->getTable();
+        $databaseConnection = $this->database->connect();
+        $allowedFields = [
+            "user_name",
+            "user_email",
+            "user_password",
+            "user_role",
+            "user_location",
+        ];
+
+        $user = array_intersect_key($user, array_flip($allowedFields));
+        $fields = array_keys($user);
+        $columns = implode(",", $fields);
+
+        $fieldsPlaceholders = array_map(fn($field) => ":{$field}", $fields);
+        $placeholders = implode(",", $fieldsPlaceholders);
+
+        $sql = "INSERT INTO {$table} ({$columns}) VALUES({$placeholders})";
+
+        $statement = $databaseConnection->prepare($sql);
+
+        return ["results" => $statement->execute($user)];
     }
 }
