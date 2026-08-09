d4fc052d896a37ce8f5f2a2a385cb07e527d0903
imsamaritan <aicodemate.talk@gmail.com>
2026-08-09 14:30:33 +0200

    UserRepository Handling User That Exists In The Database Throw "UNIQUE" constrain
    - UserRepository catching an exception if a user email exists in the
      database
    - Eliminated Email exists validation from the Database form the
      UserValidationMiddleware
    - Remove the responsibility of Database communication in the Middleware
      area
    - Change CREATE_USER_VALIDATION_FIELDS in the Helper module for email to
      allow "field" field instead of "fields"

--- src/App/Repositories/Users/UserRepository.php
+++ src/App/Repositories/Users/UserRepository.php
@@ -8,44 +8,48 @@ use App\Repositories\BaseRepository;
 use App\Interfaces\UserRepositoryInterface;
 use App\Helper\Helper;
 use App\Utilities\AuthTokenUtils;
+use PDOException;
 
 class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
     protected ?string $table = "users";
 
-    public function register(array $user): array|false
+    public function register(array $user): array
     {
-        if ($user["user_role"] === "admin") {
-            $user["pending_status"] = true;
-        } else {
-            $user["pending_status"] = null;
+        try {
+            if ($user["user_role"] === "admin") {
+                $user["pending_status"] = true;
+            } else {
+                $user["pending_status"] = null;
+            }
+
+            $user = array_intersect_key(
+                $user,
+                array_flip(Helper::INSERT_USER_ALLOWED_FIELDS),
+            );
+            $fields = array_keys($user);
+            $columns = implode(",", $fields);
+
+            $fieldsPlaceholders = array_map(fn($field) => ":{$field}", $fields);
+            $placeholders = implode(",", $fieldsPlaceholders);
+
+            $sql = "INSERT INTO {$this->table} ({$columns}) VALUES({$placeholders})";
+
+            $statement = $this->databaseConnection()->prepare($sql);
+
+            $user["user_password"] = password_hash(
+                $user["user_password"],
+                PASSWORD_DEFAULT,
+            );
+
+            $statement->execute($user);
+            return ["status" => true];
+        } catch (PDOException $e) {
+            return array_intersect_key(
+                Helper::CREATE_USER_VALIDATION_SCHEMA[0],
+                array_flip(["code", "field", "message"]),
+            );
         }
-
-        $user = array_intersect_key(
-            $user,
-            array_flip(Helper::INSERT_USER_ALLOWED_FIELDS),
-        );
-        $fields = array_keys($user);
-        $columns = implode(",", $fields);
-
-        $fieldsPlaceholders = array_map(fn($field) => ":{$field}", $fields);
-        $placeholders = implode(",", $fieldsPlaceholders);
-
-        $sql = "INSERT INTO {$this->table} ({$columns}) VALUES({$placeholders})";
-
-        $statement = $this->databaseConnection()->prepare($sql);
-
-        $user["user_password"] = password_hash(
-            $user["user_password"],
-            PASSWORD_DEFAULT,
-        );
-        $executeCreateUser = $statement->execute($user);
-
-        if (!$executeCreateUser) {
-            return ["status" => false];
-        }
-
-        return ["status" => true];
     }
 
     public function login(array $user): array
@@ -58,7 +62,10 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
         }
 
         $hashed_password = $payload["user_password"];
-        $password_verify = password_verify($user["user_password"], $hashed_password);
+        $password_verify = password_verify(
+            $user["user_password"],
+            $hashed_password,
+        );
         if ($password_verify === false) {
             return [
                 "code" => Helper::AUTH_USER_VALIDATION_SCHEMA[1]["code"],
@@ -69,6 +76,6 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
         unset($payload["user_password"]);
         $token = AuthTokenUtils::generateToken($payload);
 
-        return ["token" => "Bearer {$token}" ];
+        return ["token" => "Bearer {$token}"];
     }
 }
